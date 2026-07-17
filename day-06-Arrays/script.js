05d741a0dc50c858a8e6d7420657b71626d47a85
Izick7 <zickox491@gmail.com>
2026-07-17 07:35:01 +0100

    Introduction to arrays

---
+++ day-06-Arrays/script.js
@@ -0,0 +1,14 @@
+// 1
+const foods = ["Yam", "Rice", "Beans", "Bread", "Potato"];
+
+// 2
+let third = foods[2];
+console.log(third);
+
+// 3
+foods[0] = "Spaghetti";
+console.log(foods)
+
+// 4
+
+console.log(foods.length);
