c5a986cf4d44808219938b9cdf7fe567ae9be18f
Izick7 <zickox491@gmail.com>
2026-07-19 16:18:11 +0100

    added day 8 Array Methods

---
+++ day-08-Array-Methods/script.js
@@ -0,0 +1,10 @@
+// includes()
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(numbers.includes(3, 2));
+
+// indexOf()
+const animals = ["cat", "rat", "dog", "goat"]
+
+console.log(animals.indexOf("dog"))
