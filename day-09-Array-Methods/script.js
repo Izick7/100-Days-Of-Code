3942156fb28429055ae7ac639d0446fe2c2631f9
Izick7 <zickox491@gmail.com>
2026-07-20 16:45:54 +0100

    Added day 9 folder

---
+++ day-09-Array-Methods/script.js
@@ -0,0 +1,12 @@
+// slice()
+
+const animals = ["pig", "cow", "rat", "whale"]
+
+console.log(animals.slice(-1));
+console.log(animals);
+
+// splice()
+
+const numbers = [1,5, 4, 3, 2]
+
+console.log(numbers.splice(2, 4))
